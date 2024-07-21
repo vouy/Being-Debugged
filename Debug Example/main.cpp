df5c94d4db5f5da41a2343ce7cd4f83088665fc1
vouy <122684578+vouy@users.noreply.github.com>
2024-07-21 13:11:03 +0200

    Add files via upload

---
+++ Debug Example/main.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <iostream>
+#include <winternl.h>
+
+void CheckQueryInformation()
+{
+    typedef NTSTATUS(__stdcall* CheckQueryInformationProcess)(_In_ HANDLE, _In_  unsigned int, _Out_ PVOID, _In_ ULONG, _Out_ PULONG);
+    HANDLE Process = INVALID_HANDLE_VALUE;
+    PROCESS_BASIC_INFORMATION Info = { 0 };
+    ULONG Length = 0;
+    HMODULE Library = LoadLibraryW(L"ntdll.dll");
+    CheckQueryInformationProcess NtQueryInformationProcess = NULL;
+    NtQueryInformationProcess = (CheckQueryInformationProcess)GetProcAddress(Library, "NtQueryInformationProcess");
+    Process = GetCurrentProcess();
+    NTSTATUS Status = NtQueryInformationProcess(Process, ProcessBasicInformation, &Info, sizeof(Info), &Length);
+    if (NT_SUCCESS(Status))
+    {
+        PPEB Peb = Info.PebBaseAddress;
+        if (Peb)
+        {
+            if (Peb->BeingDebugged)
+            {
+                exit(0);
+            }
+        }
+    }
+}
+
+int main()
+{
+    CheckQueryInformation();
+	std::cin.get();
+}
